8e9fae3060ae249d2d8742e329514a3e09e50b6e
kpcruz <kelvinmembership@gmail.com>
2019-08-22 12:31:14 -0700

    updated menu-items with react-router-dom, navigating via withRouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./page/homepage/homepage.component";
 
-class App extends Component {
-	render() {
-		return (
-			<div>
-				<HomePage />
-			</div>
-		);
-	}
-}
+const App = () => {
+	return (
+		<div>
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+			</Switch>
+		</div>
+	);
+};
 
 export default App;
